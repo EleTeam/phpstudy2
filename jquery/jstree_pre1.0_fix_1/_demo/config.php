2a3b84ce8aa8e8b76d62086fc46500754c7cb926
Huazai <hi.huazai@gmail.com>
2012-07-03 09:31:06 +0800

    Add jstree

--- jquery/jstree_pre1.0_fix_1/_demo/config.php
+++ jquery/jstree_pre1.0_fix_1/_demo/config.php
@@ -2,9 +2,9 @@
 // Database config & class
 $db_config = array(
 	"servername"=> "localhost",
-	"username"	=> "root",
-	"password"	=> "",
-	"database"	=> ""
+	"username"	=> "phpstudy",
+	"password"	=> "123456",
+	"database"	=> "phpstudy",
 );
 if(extension_loaded("mysqli")) require_once("_inc/class._database_i.php"); 
 else require_once("_inc/class._database.php"); 
