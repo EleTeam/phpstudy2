5214efd949ccfe41ffc0bd9b07b080a60c232f23
Huazai <hi.huazai@gmail.com>
2012-07-06 18:24:11 +0800

    Add demo of regexp

---
+++ regexp/demo/matches.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Working with URL
+ */
+echo '<br />************ Working with URL **************<br />';
+$url = 'http://www.php.net/index.html';
+// Get host name
+$pattern = '#^(?:http://)?([^/]+)#i';
+preg_match($pattern, $url, $matches);
+$host = $matches[1];
+echo "Host name is: $host <br />";
+
+// Get domain name
+$pattern = '/[^.]+\.[^.]+$/';
+preg_match($pattern, $host, $matches);
+echo "Domain name is: {$matches[0]} <br />";
+
+// Get last part
+$pattern = '/[^\/]*$/';
+preg_match($pattern, $url, $matches);
+echo "Last part is: {$matches[0]} <br />";
+
+// Using named subpattern
